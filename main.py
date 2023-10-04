0756fdc5606198ec23d013786955fcc8e71902d2
Igor Larionov <jedialar@gmail.com>
2023-10-04 22:49:46 +0300

    edit main.py, pyproject.toml and README.md

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from database.DBCreator import run_db_creation
+from src.utils import run_user_commands
+
+
 def main():
-    pass
+    run_db_creation()
+    run_user_commands()
 
 
 if __name__ == '__main__':
